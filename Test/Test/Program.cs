beb2944045466a5394c338bcc7160b61bce31cc6
Daniel Kowalski <daniel.kowalski@modelingevolution.com>
2022-07-05 09:25:42 +0200

    Add test webapp

---
+++ Test/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using LibGit2Sharp;
+
+var repoUrl = "https://gitea.modelingevolution.com/ModelingEvolution/Tools.git";
+var dirPath = "/home/danielk/testrepo/test";
+var options = new CloneOptions()
+{
+    CredentialsProvider = (_, _, _) => new UsernamePasswordCredentials
+    {
+        Username = "daniel.kowalski@modelingevolution.com",
+        Password = "D@nK0w18git",
+    }
+};
+
+//var repo = Repository.Clone(repoUrl, dirPath, options);
+
+var x = Directory.EnumerateFiles(dirPath, "*.csproj", SearchOption.AllDirectories);
+
+
+
+Console.WriteLine(Path.DirectorySeparatorChar);
