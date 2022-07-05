06aff27e89f2bd0bc88b8e533bae9ba490ff9bdb
Daniel Kowalski <daniel.kowalski@modelingevolution.com>
2022-07-05 09:55:09 +0200

    Add ping

--- Test/WebApplication1/Program.cs
+++ Test/WebApplication1/Program.cs
@@ -1,3 +1,6 @@
+using System.Net.NetworkInformation;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +24,30 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+Ping pingSender = new Ping ();
+PingOptions options = new PingOptions ();
+
+// Use the default Ttl value which is 128,
+// but change the fragmentation behavior.
+options.DontFragment = true;
+
+// Create a buffer of 32 bytes of data to be transmitted.
+string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+byte[] buffer = Encoding.ASCII.GetBytes (data);
+int timeout = 120;
+PingReply reply = pingSender.Send ("evenstore", timeout, buffer, options);
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine ("Address: {0}", reply.Address.ToString ());
+    Console.WriteLine ("RoundTrip time: {0}", reply.RoundtripTime);
+    Console.WriteLine ("Time to live: {0}", reply.Options.Ttl);
+    Console.WriteLine ("Don't fragment: {0}", reply.Options.DontFragment);
+    Console.WriteLine ("Buffer size: {0}", reply.Buffer.Length);
+}
+else
+{
+    Console.WriteLine("Nie udało się");
+}
 
 app.Run();
